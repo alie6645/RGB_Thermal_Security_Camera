d33d4065682a1eaf6bf99ca7f2558200e16b91de
derpi <derpi@ACOMPUTER>
2026-03-05 15:29:33 -0700

    add comparison and img mod scripts

--- olddata/tinygen.py
+++ olddata/tinygen.py
@@ -1,7 +1,7 @@
 import cv2
 import os
-
-for i in range (2443, 2543):
+count = 0
+for i in range (2443, 3443, 10):
     num = i - 2443
     name = str(i) + ".jpg"
     rgbpath = os.path.join("dataset_1-6-2024", "class1", name)
@@ -10,7 +10,8 @@ for i in range (2443, 2543):
     therm = cv2.imread(thermpath)
     rgb = cv2.resize(rgb, (32, 32), interpolation=cv2.INTER_AREA)
     therm = cv2.resize(therm, (32, 32), interpolation=cv2.INTER_AREA)
-    name = str(num) + ".jpg"
+    name = str(count) + ".jpg"
+    count = count + 1
     rgbpath = os.path.join("tiny", "test", "rgb", name)
     thermpath = os.path.join("tiny", "test", "therm", name)
     cv2.imwrite(rgbpath, rgb)
